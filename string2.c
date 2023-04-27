302d7b5d11277b445a5ce2b696ea06cfc928614e
Initech <inimfonebong001@gmail.com>
2023-04-27 13:09:33 -0700

    more update

---
+++ string2.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
+
+/**
+ * _strncmp - compares two strings
+ * @s1: first sring
+ * @s2: string 2
+ * @n: lem
+ * Return: 0 or 1
+ */
+
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	for (i = 0; i < n; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
