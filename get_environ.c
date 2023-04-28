6fea8f1936e1cf8cab6b4f68c679a90909f1b520
Initech <inimfonebong001@gmail.com>
2023-04-28 05:33:55 -0700

    updates

--- get_environ.c
+++ get_environ.c
@@ -14,8 +14,7 @@ void get_environ_var(void)
 		len = _strlen(environ[i]);
 
 		write(1, environ[i], len);
-	/*	if (environ[++i] != NULL)*/
+		if (environ[++i] != NULL)
 			write(1, "\n", 1);
-			environ++;
 	}
 }
